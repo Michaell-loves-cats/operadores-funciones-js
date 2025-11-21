c5b6c24e36443885df976938123cb1dcf6ad603a
Hellen Ruiz <estefany25rb@gmail.com>
2025-11-21 14:09:53 -0500

    eliminando los archivos que no eran necesarios

--- assets/js/operadores_logicos.js
+++
@@ -1,9 +0,0 @@
-/* Operadores lógicos
-*
-*/
-let x = 6;
-let y = 3;
-
-document.getElementById("demo2").innerHTML = 
-(x < 10 && y > 1) + "<br>" +
-(x < 10 && y < 1);
--- assets/js/operadores_relacionales.js
+++
@@ -1,6 +0,0 @@
-/**Operadores relacionales
- * 
- */
-
-let x = 5;
-document.getElementById("demo1").innerHTML = (x >= 8); //false
