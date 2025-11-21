f1b79f2bea05b332b74a63275624f94c3f041b49
Hellen Ruiz <estefany25rb@gmail.com>
2025-11-21 14:42:15 -0500

    Corregir después de la aclaración

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -1,8 +1,9 @@
 /* ¿QUÉ ES UNA FUNCIÓN
 * Serie de intrucciones que tiene o no parametros de entrada
-* function NombreDeLaFunción(Parametros){
-    Serie de intrucciones
-}
+* function NombreDeLaFunción(Parametros)
+*{
+*Serie de intrucciones
+*}
 */
 
 function myFunction(){
