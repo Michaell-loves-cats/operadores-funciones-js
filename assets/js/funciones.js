5f8dac9ee8e53e7486deaa37a68f32ab93ae976c
Hellen Ruiz <estefany25rb@gmail.com>
2025-11-21 13:48:58 -0500

    Versión de clase anterior

---
+++ assets/js/funciones.js
@@ -0,0 +1,10 @@
+/* ¿QUÉ ES UNA FUNCIÓN
+* Serie de intrucciones que tiene o no parametros de entrada
+* function NombreDeLaFunción(Parametros){
+    Serie de intrucciones
+}
+*/
+
+function myFunction(){
+document.getElementById("demo3").innerHTML = "Michaell Benavides";
+}
